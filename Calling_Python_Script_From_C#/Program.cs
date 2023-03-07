713ca759f9e8a7397cdb8ae24a61c80610cf9414
Ausha M S <anusgowda050@gmail.com>
2023-03-07 11:00:14 +0530

    Creating Console Application.

--- Calling_Python_Script_From_C#/Program.cs
+++ Calling_Python_Script_From_C#/Program.cs
@@ -1,2 +1,15 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+//Console.WriteLine("Hello, World!");
+using IronPython.Hosting;
+
+namespace ConsoApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("hi");
+            var engine = Python.CreateEngine();
+        }
+    }
+}
