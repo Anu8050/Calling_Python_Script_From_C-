3d8d3f831b89078dff18143a9de5f4800f451924
Ausha M S <anusgowda050@gmail.com>
2023-03-07 11:22:45 +0530

    Calling python Script from C# console application.

--- Calling_Python_Script_From_C#/Program.cs
+++ Calling_Python_Script_From_C#/Program.cs
@@ -1,15 +1,26 @@
 ﻿// See https://aka.ms/new-console-template for more information
 //Console.WriteLine("Hello, World!");
+using ComponentAce.Compression.Libs.ZLib;
 using System.Diagnostics;
 
-namespace ConsoApp
+class Program
 {
-    class Program
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("hi");
+        //Console.WriteLine("hi");
             
+        ProcessStartInfo start = new ProcessStartInfo();
+        start.FileName = @"C:\Users\User\AppData\Local\Programs\Python\Python310\python.exe";
+        start.Arguments = @"F:\\C#Example\\PythonFile\\print.py";
+        start.UseShellExecute = false;
+        start.CreateNoWindow = true;
+        start.RedirectStandardOutput = true;
+        string result = "";
+        using (Process process = Process.Start(start))
+        {
+            result = process.StandardOutput.ReadToEnd();
         }
+        Console.WriteLine(result);
     }
 }
+
